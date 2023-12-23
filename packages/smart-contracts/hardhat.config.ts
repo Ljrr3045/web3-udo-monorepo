71132bd42289b3af4c576018d8965fb507e0fc46
Ljrr3045 <ljrr3045@gmail.com>
2023-12-23 17:53:14 -0400

    usuing new frame

--- packages/smart-contracts/hardhat.config.ts
+++ packages/smart-contracts/hardhat.config.ts
@@ -1,41 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@openzeppelin/hardhat-upgrades";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "hardhat-contract-sizer";
-import "hardhat-dependency-compiler";
-import "hardhat-deploy";
-import "hardhat-deploy-ethers";
-import "@nomiclabs/hardhat-web3";
-
-require("hardhat-dependency-compiler");
-
-import networks from "./hardhat.networks";
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-        {
-            version: "0.8.21",
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 1000000,
-                },
-            },
-        }
-    ],
-  },
-  networks,
-  namedAccounts: {
-    deployer: 0,
-    admin: 1,
-    minter: 2,
-    user: 3
-  }
+  solidity: "0.8.19",
 };
 
 export default config;
