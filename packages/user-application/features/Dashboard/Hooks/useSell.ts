a2e95ab5553c6cafbdde3cafc4ce9810877bdf5c
Ljrr3045 <ljrr3045@gmail.com>
2023-12-30 19:09:40 -0400

    sell flow completed

---
+++ packages/user-application/features/Dashboard/Hooks/useSell.ts
@@ -0,0 +1,56 @@
+import UDOT_ABI from "../../../utils/ABIs/UDOT.json";
+import { UDOTAddress } from "../../../utils/Constants";
+import { useEffect } from "react";
+import { ethers } from "ethers";
+import { toast } from "react-toastify";
+import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+
+export const useSell = ({
+  value
+}:{
+  value: string
+}) => {
+  const burnAmount = value !== "" ? value : "0.0";
+
+/* Hooks interactions */
+  const { config } = usePrepareContractWrite({
+    address: UDOTAddress,
+    abi: UDOT_ABI,
+    functionName: "burn",
+    args: [ethers.parseEther(burnAmount)]
+  });
+
+  const { data, write } = useContractWrite(config);
+
+  const { isLoading, isSuccess, isError } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+/* Internal functions */
+  const sendTransaction = () => {
+    if (write) write();
+  }
+
+/* Effects */
+    useEffect(() => {
+      if (isLoading) {
+        toast.info("Transaction in progress");
+      }
+    }, [isLoading]);
+
+    useEffect(() => {
+      if (isSuccess) {
+        toast.success("Transaction completed satisfactorily");
+      }
+    }, [isSuccess]);
+
+    useEffect(() => {
+      if (isError) {
+        toast.error("Transaction failed");
+      }
+    }, [isError]);
+
+  return {
+    sendTransaction,
+  };
+}
