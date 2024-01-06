7eca6a8218bc3e6d0d3463bdc9ca32f279c6ce37
Ljrr3045 <ljrr3045@gmail.com>
2024-01-06 17:46:29 -0400

    blocked vote view

--- packages/user-application/features/Vote/VoteBlocked.tsx
+++ packages/user-application/features/Vote/VoteBlocked.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { Card } from "../../components/Common/Card";
+import { CgSmileSad } from "react-icons/cg";
+import { Button } from "../../components/Common/Button";
 
 export const VoteBlocked = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      VoteBlocked
-    </div>
+    <Card>
+      <div className="w-full flex flex-col items-center justify-center gap-2">
+        <CgSmileSad size={100} className="text-blue-600" />
+        <p className="text-base font-normal text-black text-center">
+          <b>{`Sorry...`}</b>
+          {` your balance is not enough to be able to vote, `}
+          <b>{`you must have at least 1 UDOT in your wallet.`}</b>
+        </p>
+      </div>
+      <Button
+        text="Come Back to Dashboard"
+        onClick={() => router.push("/dashboard")}
+      />
+    </Card>
   );
 }
