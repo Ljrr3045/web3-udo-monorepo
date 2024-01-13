21cbf3626794c239e5bf6fd344c89f3e6e0bb01e
Ljrr3045 <ljrr3045@gmail.com>
2024-01-13 18:18:59 -0400

    create routes

---
+++ packages/user-application/pages/transactionHistory.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { NextPage } from "next";
+
+const TransactionHistory: NextPage = () => {
+  return (
+    <div>
+      TransactionHistory
+    </div>
+  );
+}
+
+export default TransactionHistory;
